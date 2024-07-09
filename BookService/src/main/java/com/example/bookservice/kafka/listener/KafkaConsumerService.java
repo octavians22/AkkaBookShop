30cc941e9c845f1b097d27da9ad07bd923343153
octavians22 <89181763+octavians22@users.noreply.github.com>
2024-07-09 13:12:48 +0300

    kafka dlt - not working

--- BookService/src/main/java/com/example/bookservice/kafka/listener/KafkaConsumerService.java
+++ BookService/src/main/java/com/example/bookservice/kafka/listener/KafkaConsumerService.java
@@ -3,6 +3,7 @@ package com.example.bookservice.kafka.listener;
 
 import com.example.bookservice.dto.ReviewDTO;
 import com.example.bookservice.exceptions.BookNotFoundException;
+import com.example.bookservice.kafka.producer.KafkaProducerService;
 import com.example.bookservice.mapper.ReviewMapper;
 import com.example.bookservice.model.Book;
 import com.example.bookservice.model.Review;
@@ -10,9 +11,17 @@ import com.example.bookservice.repository.BookRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.DltStrategy;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 import java.util.List;
+
+import static org.apache.kafka.common.requests.DeleteAclsResponse.log;
 
 @Service
 public class KafkaConsumerService {
@@ -20,36 +29,58 @@ public class KafkaConsumerService {
 		private final ObjectMapper objectMapper = new ObjectMapper();
 		private final BookRepository bookRepository;
 		private final ReviewMapper reviewMapper;
+		private final KafkaProducerService kafkaProducerService;
 		private List<Review> reviewList;
 
-		public KafkaConsumerService(BookRepository bookRepository, ReviewMapper reviewMapper) {
+		public KafkaConsumerService(BookRepository bookRepository, ReviewMapper reviewMapper, KafkaProducerService kafkaProducerService) {
 				this.bookRepository = bookRepository;
 				this.reviewMapper = reviewMapper;
+				this.kafkaProducerService = kafkaProducerService;
 		}
 
+		@RetryableTopic(attempts = "4",
+						backoff = @Backoff(delay = 1500, multiplier = 2),
+						kafkaTemplate = "kafkaTemplate",
+						dltStrategy = DltStrategy.ALWAYS_RETRY_ON_ERROR)
 		@KafkaListener(topics = "book-reviews", groupId = "book-group")
 		public void listenReviews(ConsumerRecord<String, String> record) {
 
 				try {
-						ReviewDTO reviewDTO = objectMapper.readValue(record.value(), ReviewDTO.class);
+//						ReviewDTO reviewDTO = objectMapper.readValue(record.value(), ReviewDTO.class);
+//
+//
+//						Book book = bookRepository.findByTitleAndAuthorAndPublisher(reviewDTO.getBookIdentifierDTO().getTitle(),
+//										reviewDTO.getBookIdentifierDTO().getAuthor(),
+//										reviewDTO.getBookIdentifierDTO().getPublisher())
+//										.orElseThrow(() -> new BookNotFoundException("This book no longer exists to add a review"));
+//
+//						reviewList = book.getReviews();
+//
+//						if(!reviewAlreadySent(reviewDTO)) {
+//								return;
+//						}
+//
+//						Review review = reviewMapper.toEntity(reviewDTO);
+//
+//								book.getReviews().add(review);
+//								bookRepository.save(book);
+						throw new Exception("exception");
 
-						Book book = bookRepository.findByTitleAndAuthorAndPublisher(reviewDTO.getBookIdentifierDTO().getTitle(),
-										reviewDTO.getBookIdentifierDTO().getAuthor(),
-										reviewDTO.getBookIdentifierDTO().getPublisher())
-										.orElseThrow(() -> new BookNotFoundException("This book no longer exists to add a review"));
+				}  catch (Exception e) {
+						log.info(e.getMessage());
+						handleDltPayment(record, "book-reviews-dlt");
+				}
+		}
 
-						reviewList = book.getReviews();
+		@DltHandler
+		public void handleDltPayment(ConsumerRecord<String, String> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
 
-						if(!reviewAlreadySent(reviewDTO)) {
-								return;
-						}
-
-						Review review = reviewMapper.toEntity(reviewDTO);
-						book.getReviews().add(review);
-						bookRepository.save(book);
-
-				} catch (JsonProcessingException e) {
-						throw new RuntimeException(e);
+				try {
+						ReviewDTO	reviewDTO = objectMapper.readValue(record.value(), ReviewDTO.class);
+						log.info("Event on dlt topic={}, payload={}", topic, reviewDTO);
+						kafkaProducerService.sendMessage("book-reviews", reviewDTO);
+				} catch (Exception e) {
+						log.error("Error processing DLT message: ", e);
 				}
 		}
 
